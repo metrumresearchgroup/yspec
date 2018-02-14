a81de4682c228f2499b7907133a77c5daf8a749a
Kyle Baron <kyleb@metrumrg.com>
2018-02-14 13:42:49 -0600

    adding some extra messages per request #4

--- R/check_spec.R
+++ R/check_spec.R
@@ -1,17 +1,27 @@
 
-check_values <- function(x,values) {
+check_values <- function(x,values,verbose=FALSE) {
   if(is.null(values)) return(TRUE)
   x <- x[!is.na(x)]
   x <- unlist(unique(x),use.names = FALSE)
+  if(verbose) {
+    valu <- values
+    if(length(values) > 3) {
+      valu <- c(valu[seq(3)], "...")
+    }
+    message("  values: ", paste0(valu, collapse = ','))
+  }
   if(length(x) != length(values)) return(FALSE)
   length(setdiff(x,values))==0
 }
 
-check_range <- function(x,range) {
+check_range <- function(x,range,verbose=FALSE) {
   if(is.null(range)) return(TRUE)
   if(length(range) !=2) return(FALSE)
   x <- x[!is.na(x)]
   if(length(x)==0) return(TRUE)
+  if(verbose) {
+    message("  range: ", paste0(range, collapse = ","))
+  }
   x <- sort(range(x))
   range <- sort(range)
   x[1] >= range[1] & x[2] <= range[2]
@@ -34,20 +44,26 @@ add_error <- function(env) {
 ##'
 ##' @param data a data frame
 ##' @param spec a yspec object
+##' @param verbose \code{logical}; if \code{TRUE}, extra messages
+##' are printed during the check
 ##' @param file the full path to a yaml specification file
 ##' @export
-check_data <- function(data, spec) {
+check_data <- function(data, spec, verbose = FALSE) {
   env <- new.env()
   env$log <- character(0)
   env$error <- FALSE
   nspec <- names(spec)
   ndata <- names(data)
   data <- as.data.frame(data)
-
+  
+  if(verbose) {
+    message("checking that names in the spec and the data set match")
+  }
+  
   if(!identical(nspec, ndata)) {
     add_log(env, "data names do not match names in spec")
     add_error(env)
-
+    
     if(length(nspec) > length(ndata)) {
       add_log(env, "spec has more items that data")
     }
@@ -55,22 +71,25 @@ check_data <- function(data, spec) {
       add_log(env, "data has more items that spec")
     }
   }
-
+  
   for(i in seq_along(spec)) {
     x <- spec[[i]]
     y <- data[,x$col]
-    val <- check_values(y,x$values)
+    if(verbose) message("checking column: ", x$col)
+    val <- check_values(y,x$values,verbose=verbose)
     if(!val) {
-      add_log(env, "discrete column value out of range: ", x$col)
+      add_log(env, "discrete value out of range:", x$col)
       add_error(env)
     }
-    range <- check_range(y,x$range)
+    range <- check_range(y,x$range,verbose=verbose)
     if(!range) {
-      add_log(env, "continuous column value out of range: ", x$col)
+      range <- paste0(x$range,collapse = ",")
+      range <- paste0("[",range,"]")
+      add_log(env, "continuous value out of range:", x$col, range)
       add_error(env)
     }
   }
-
+  
   if(env$error) {
     message("")
     message("Messages:")
@@ -79,7 +98,7 @@ check_data <- function(data, spec) {
     .stop("please review messages and re-check")
   }
   message("Everything checks out!")
-
+  
   return(invisible(env$error))
 }
 
