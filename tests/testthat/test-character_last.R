27077de035a1737e64c43e5ba482789210e12f58
Kyle Baron <kyleb@metrumrg.com>
2020-06-12 12:52:56 -0500

    tweak tests

--- tests/testthat/test-character_last.R
+++ tests/testthat/test-character_last.R
@@ -3,11 +3,9 @@ library(testthat)
 
 context("test-character_last")
 
-.sp <- yspec:::test_spec_test
-
 test_that("put character columns last, except comment_col", {
-  spec <- .sp("character_last-issue-86.yml")
-  type <- unname(map_chr(spec,"type"))
+  spec <- yspec:::test_spec_test("character_last-issue-86.yml")
+  type <- unname(purrr::map_chr(spec,"type"))
   n <- length(type)
   terminal_chr <- seq((n-2),n)
   expect_true(all(type[terminal_chr]=="character"))
