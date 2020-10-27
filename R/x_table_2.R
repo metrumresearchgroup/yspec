ef476e6ccd1ca3b65cfb81f405f38c91ba1a26e7
Kyle Baron <kyleb@metrumrg.com>
2020-10-27 12:33:47 -0500

    don't center table; tweak column / table width

--- R/x_table_2.R
+++ R/x_table_2.R
@@ -48,7 +48,7 @@ x_table_2 <- function(spec,
     tab, 
     align = c("p{0cm}",
               "|p{0.8in}" , 
-              ">{\\raggedright\\arraybackslash}p{2.42in}",
+              ">{\\raggedright\\arraybackslash}p{2.39in}",
               ">{\\raggedright\\arraybackslash}p{2.78in}|")
   )
   add.to.row <- list(pos = list(0), command = NULL)
@@ -64,6 +64,7 @@ x_table_2 <- function(spec,
   pxt <- xtable::print.xtable(
     xt, 
     tabular.environment = "longtable", 
+    latex.environments = NULL,
     include.rownames = FALSE,
     hline.after = c(-1,hlines),
     comment = FALSE,
