dad5d815a0063b88cbb4d2a01fd27f1ae7b72d84
Kyle Baron <kyleb@metrumrg.com>
2022-01-03 09:18:22 -0600

    roll back expectation on final result for col order

--- tests/testthat/test-prune.R
+++ tests/testthat/test-prune.R
@@ -16,7 +16,7 @@ test_that("ys_prune selects available columns", {
   spec2 <- ys_select(spec, -STUDY, -TAD)
   expect_identical(class(data), class(ans))
   expect_true(is.data.frame(ans))
-  expect_length(setdiff(names(ans), names(spec2)), 0)
+  expect_equal(names(ans), names(spec2))
   expect_error(
     ys_prune(data.frame(a = 2), spec), 
     regexp = "there are no names common between"
