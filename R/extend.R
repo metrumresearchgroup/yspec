a34005b14146221a8ee5ae22f88330e7261981b0
Kyle Baron <kyleb@metrumrg.com>
2022-02-12 17:18:42 -0600

    public function to extend

--- R/extend.R
+++ R/extend.R
@@ -1,20 +1,32 @@
 
-ys_load_extend <- function(x) {
-  extend_file <- maybe_pull_meta(x, "extend_file")
-  if(is.null(extend_file)) {
-    stop(
-      "There was no `extend_file` specified in the `SETUP__` block; ", 
-      "cannot extend this yspec object."
-    )
+#' Extend a yspec object
+#' 
+#' Use this function to read and join another spec file to an existing 
+#' `yspec` object. The additional spec file can contain additional columns
+#' that might be added to the data set on an ad-hoc basis or could include
+#' modeling outputs (e.g. `IPRED`).
+#' 
+#' @details
+#' The extension `yspec` object cannot contain any columns in common with the 
+#' primary `yspec` object. 
+#' 
+#' @param x A `yspec` object. 
+#' @param file The path to a yaml specification file to load and join to `x`. 
+#' 
+#' @examples
+#' extension_file <- system.file("spec", "nm-extension.yml")
+#' 
+#' spec <- yys_help$spec() 
+#' spec <- ys_extend(spec, extension_file)
+#' tail(spec)
+#' 
+#' @md
+#' @export
+ys_extend <- function(x, file) {
+  if(!file.exists(file)) {
+    stop("Extension file does not exist: ", file)
   }
-  if(!is.character(extend_file)) {
-    stop("`extend_file` must have type `character`.")  
-  }
-  extend_file <- file.path(pull_meta(x, "spec_path"), extend_file)
-  if(!file.exists(extend_file)) {
-    stop("`extend_file` does not exist: ", extend_file)
-  }
-  extension <- ys_load(extend_file, verbose = FALSE, extend = FALSE)
+  extension <- ys_load(file, verbose = FALSE, extend = FALSE)
   if(any(names(extension) %in% names(x))) {
     message <- "\nNames in extension cannot also exist in the base spec:"
     common <- intersect(names(extension), names(x))
@@ -23,6 +35,20 @@ ys_load_extend <- function(x) {
     all <- paste0(all, "\n")
     stop(all, call. = FALSE)  
   }
-  c(x, extension) 
+  c(x, extension)    
 }
 
+ys_load_extend <- function(x) {
+  extend_file <- maybe_pull_meta(x, "extend_file")
+  if(is.null(extend_file)) {
+    stop(
+      "There was no `extend_file` specified in the `SETUP__` block; ", 
+      "cannot extend this yspec object."
+    )
+  }
+  if(!(is.character(extend_file) && length(extend_file)==1)) {
+    stop("`extend_file` must be a single string.")  
+  }
+  extend_file <- file.path(pull_meta(x, "spec_path"), extend_file)
+  ys_extend(x, extend_file)
+}
--- tests/testthat/test-extend.R
+++ tests/testthat/test-extend.R
@@ -36,6 +36,17 @@ test_that("load spec with extension", {
   expect_equal(names(spec), LETTERS[1:2])
 })
 
+test_that("extend a yspec object", {
+  extension_file <- system.file("spec", "nm-extension.yml",  package = "yspec")
+  spec <- ys_help$spec()
+  prev <- names(spec)
+  spec <- ys_extend(spec, extension_file)
+  expect_is(spec, "yspec")
+  diff <- setdiff(names(spec), prev)
+  ext <- ys_load(extension_file)
+  expect_identical(diff, names(ext))
+})
+
 test_that("extension fails when common columns", {
   ext <- yspec:::temp_spec(ext_bad, "foo.yml")
   expect_error( 
@@ -49,6 +60,6 @@ test_that("extension fails when extension file doesn't exist", {
   unlink(ext, recursive = TRUE)
   expect_error(
     ys_load(file, extend = TRUE), 
-    regexp = "`extend_file` does not exist"
+    regexp = "Extension file does not exist"
   )
 })
