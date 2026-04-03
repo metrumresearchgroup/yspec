210b73cbc00a6c94119fcc4fefea8be9bae91435
Kyle Baron <kyleb@metrumrg.com>
2026-04-03 15:43:19 -0500

    try namespace when labeling xpt

--- R/class-yspec.R
+++ R/class-yspec.R
@@ -412,9 +412,11 @@ yspec_yml_file.default <- function(x,...) {
 #' @param data a `data.frame` to label.
 #' @param spec yspec object for `data`.
 #' @param fun the function to use for forming `label`.
-#' @param strict if `TRUE`, generate an error when `data` names are not identical
-#' to `spec` names (including order); otherwise, only data items in common between 
-#' `data` and `spec` (in any order) will be labeled.
+#' @param strict if `TRUE`, generate an error when `data` names are not 
+#' identical to `spec` names (including order); otherwise, only data items in 
+#' common between `data` and `spec` (in any order) will be labeled.
+#' @param ns character vector of namespaces to try to invoke prior to labeling
+#' the data set.
 #' 
 #' @details
 #' By default, an error is generated when the column names of the data set 
@@ -439,12 +441,15 @@ yspec_yml_file.default <- function(x,...) {
 #' 
 #' @md
 #' @export
-ys_add_labels <- function(data, spec, fun = label.ycol, strict = TRUE) {
-  assert_that(inherits(data,"data.frame"))
-  assert_that(inherits(spec,"yspec"))
+ys_add_labels <- function(data, spec, fun = label.ycol, strict = TRUE, 
+                          ns = c("define", "label_xpt")) {
+  assert_that(inherits(data, "data.frame"))
+  assert_that(inherits(spec, "yspec"))
+  assert_that(is.character(ns) || is.null(ns))
   if(isTRUE(strict)) {
     assert_that(identical(names(data), names(spec)))  
   }
+  spec <- try_this_namespace(spec, ns)
   col_labels <- map_chr(spec, fun)
   col_labels <- col_labels[names(col_labels) %in% names(data)]
   if(!length(col_labels)) {
