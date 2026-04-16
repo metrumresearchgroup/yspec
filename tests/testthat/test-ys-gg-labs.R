a15872f7da6f687f20ca806212f8442385b04ff7
Kyle Baron <kyleb@metrumrg.com>
2026-04-16 16:30:59 -0500

    better access to labels in tests

--- tests/testthat/test-ys-gg-labs.R
+++ tests/testthat/test-ys-gg-labs.R
@@ -14,48 +14,48 @@ labs <- list(
   TIME = "Time after first dose"
 )
 
-p0 <- 
-  ggplot2::ggplot(data, ggplot2::aes(TIME, DV)) + 
+p0 <-
+  ggplot2::ggplot(data, ggplot2::aes(TIME, DV)) +
   ggplot2::geom_point()
 
 test_that("label x- and y- from the spec", {
-  expect_null(p0@labels$x)
-  expect_null(p0@labels$y)
+  expect_null(ggplot2::get_labs(p0)$x)
+  expect_null(ggplot2::get_labs(p0)$y)
 
   p <- p0 + ys_gg_labs(spec)
-  expect_equal(p@labels$x, "Time (hour)")
-  expect_equal(p@labels$y, "Concentration (ng/mL)")
+  expect_equal(ggplot2::get_labs(p)$x, "Time (hour)")
+  expect_equal(ggplot2::get_labs(p)$y, "Concentration (ng/mL)")
 })
 
-test_that("labs overrides spec for x- and y-", { 
+test_that("labs overrides spec for x- and y-", {
   p <- p0 + ys_gg_labs(spec, labs)
-  expect_equal(p@labels$x, "Time after first dose")
-  expect_equal(p@labels$y, "Concentration (ng/mL)")
+  expect_equal(ggplot2::get_labs(p)$x, "Time after first dose")
+  expect_equal(ggplot2::get_labs(p)$y, "Concentration (ng/mL)")
 })
 
-test_that("force in x- or y-", { 
+test_that("force in x- or y-", {
   p <- p0 + ys_gg_labs(spec, labs, x = "A", y = "B")
-  expect_equal(p@labels$x, "A")
-  expect_equal(p@labels$y, "B")
+  expect_equal(ggplot2::get_labs(p)$x, "A")
+  expect_equal(ggplot2::get_labs(p)$y, "B")
 })
 
 test_that("transformed x- or y- is ignored", {
-  p0 <- 
-    ggplot2::ggplot(data, ggplot2::aes(TIME, log(DV))) + 
+  p0 <-
+    ggplot2::ggplot(data, ggplot2::aes(TIME, log(DV))) +
     ggplot2::geom_point()
   p <- p0 + ys_gg_labs(spec)
-  expect_equal(p@labels$x, "Time (hour)")
-  expect_equal(p@labels$y, "log(DV)")
+  expect_equal(ggplot2::get_labs(p)$x, "Time (hour)")
+  expect_equal(ggplot2::get_labs(p)$y, "log(DV)")
 })
 
 test_that("transformed x- or y- in labs", {
   labs[["log(DV)"]] <- "log-transformed DV"
-  p0 <- 
-    ggplot2::ggplot(data, ggplot2::aes(TIME, log(DV))) + 
+  p0 <-
+    ggplot2::ggplot(data, ggplot2::aes(TIME, log(DV))) +
     ggplot2::geom_point()
   p <- p0 + ys_gg_labs(spec, labs)
-  expect_equal(p@labels$x, "Time after first dose")
-  expect_equal(p@labels$y, "log-transformed DV")
+  expect_equal(ggplot2::get_labs(p)$x, "Time after first dose")
+  expect_equal(ggplot2::get_labs(p)$y, "log-transformed DV")
 })
 
 test_that("factor x- or y- is passed through", {
@@ -63,8 +63,8 @@ test_that("factor x- or y- is passed through", {
     ggplot2::ggplot(data, ggplot2::aes(factor(CP), DV)) +
     ggplot2::geom_boxplot()
   p <- p0 + ys_gg_labs(spec)
-  expect_equal(p@labels$x, "Child-Pugh score")
-  expect_equal(p@labels$y, "Concentration (ng/mL)")
+  expect_equal(ggplot2::get_labs(p)$x, "Child-Pugh score")
+  expect_equal(ggplot2::get_labs(p)$y, "Concentration (ng/mL)")
 })
 
 test_that("label colour", {
@@ -72,9 +72,9 @@ test_that("label colour", {
     ggplot2::ggplot(data, ggplot2::aes(TIME, DV, colour = factor(CP))) +
     ggplot2::geom_point()
   p <- p0 + ys_gg_labs(spec)
-  expect_equal(p@labels$colour, "Child-Pugh score")
+  expect_equal(ggplot2::get_labs(p)$colour, "Child-Pugh score")
   p <- p0 + ys_gg_labs(spec, colour = "test colour")
-  expect_equal(p@labels$colour, "test colour")
+  expect_equal(ggplot2::get_labs(p)$colour, "test colour")
 })
 
 test_that("label fill", {
@@ -82,9 +82,9 @@ test_that("label fill", {
     ggplot2::ggplot(data, ggplot2::aes(TIME, DV, fill = factor(CP))) +
     ggplot2::geom_boxplot()
   p <- p0 + ys_gg_labs(spec)
-  expect_equal(p@labels$fill, "Child-Pugh score")
+  expect_equal(ggplot2::get_labs(p)$fill, "Child-Pugh score")
   p <- p0 + ys_gg_labs(spec, fill = "test fill")
-  expect_equal(p@labels$fill, "test fill")
+  expect_equal(ggplot2::get_labs(p)$fill, "test fill")
 })
 
 test_that("label linetype", {
@@ -92,9 +92,9 @@ test_that("label linetype", {
     ggplot2::ggplot(data, ggplot2::aes(TIME, DV, linetype = factor(CP))) +
     ggplot2::geom_line()
   p <- p0 + ys_gg_labs(spec)
-  expect_equal(p@labels$linetype, "Child-Pugh score")
+  expect_equal(ggplot2::get_labs(p)$linetype, "Child-Pugh score")
   p <- p0 + ys_gg_labs(spec, linetype = "test linetype")
-  expect_equal(p@labels$linetype, "test linetype")
+  expect_equal(ggplot2::get_labs(p)$linetype, "test linetype")
 })
 
 test_that("col, color, and colour are equivalent", {
@@ -104,8 +104,8 @@ test_that("col, color, and colour are equivalent", {
   p1 <- p0 + ys_gg_labs(spec, colour = "test colour")
   p2 <- p0 + ys_gg_labs(spec, col = "test colour")
   p3 <- p0 + ys_gg_labs(spec, color = "test colour")
-  expect_equal(p1@labels$colour, p2@labels$colour)
-  expect_equal(p1@labels$colour, p3@labels$colour)
+  expect_equal(ggplot2::get_labs(p1)$colour, ggplot2::get_labs(p2)$colour)
+  expect_equal(ggplot2::get_labs(p1)$colour, ggplot2::get_labs(p3)$colour)
 })
 
 test_that("lty and linetype are equivalent", {
@@ -114,7 +114,7 @@ test_that("lty and linetype are equivalent", {
     ggplot2::geom_line()
   p1 <- p0 + ys_gg_labs(spec, linetype = "test linetype")
   p2 <- p0 + ys_gg_labs(spec, lty = "test linetype")
-  expect_equal(p1@labels$linetype, p2@labels$linetype)
+  expect_equal(ggplot2::get_labs(p1)$linetype, ggplot2::get_labs(p2)$linetype)
 })
 
 test_that("label shape", {
@@ -122,7 +122,7 @@ test_that("label shape", {
     ggplot2::ggplot(data, ggplot2::aes(TIME, DV, shape = factor(CP))) +
     ggplot2::geom_point()
   p <- p0 + ys_gg_labs(spec)
-  expect_equal(p@labels$shape, "Child-Pugh score")
+  expect_equal(ggplot2::get_labs(p)$shape, "Child-Pugh score")
   p <- p0 + ys_gg_labs(spec, shape = "test shape")
-  expect_equal(p@labels$shape, "test shape")
+  expect_equal(ggplot2::get_labs(p)$shape, "test shape")
 })
